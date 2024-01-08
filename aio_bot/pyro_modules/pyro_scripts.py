7c91186981c8508df0642409361a6a0f5fccef11
Serio <SeriousNuts@yandex.ru>
2024-01-08 08:46:23 +0300

    add time to logs

--- aio_bot/pyro_modules/pyro_scripts.py
+++ aio_bot/pyro_modules/pyro_scripts.py
@@ -75,34 +75,34 @@ async def send_message_to_tg(ch, text_message):
     except FloodWait as e:
         if app.is_connected:
             if e.value < 5000:
-                logging.error(f"{str(ch)} FLOOD WAIT MESSAGE WILL BE SENNDED IN LESS {e.value} SECONDS")
+                logging.error(f"{datetime.now()} : {str(ch)} FLOOD WAIT MESSAGE WILL BE SENNDED IN LESS {e.value} SECONDS")
                 sended_message.set_flood_wait_time(e.value)
                 await asyncio.sleep(e.value)
                 await app.send_message(str(ch).replace("https://t.me/", ""), text_message)
                 sended_message.set_message(text=text_message, sending_date=datetime.now(), status=0, channel=ch)
                 await asyncio.sleep(sleep_time)
             else:
-                logging.error(f"{str(ch)} FLOOD WAIT {e.value} NOT SENDED")
+                logging.error(f"{datetime.now()} : {str(ch)} FLOOD WAIT {e.value} NOT SENDED")
                 sended_message.set_message(text=text_message, sending_date=datetime.now(), status=3, channel=ch)
                 sended_message.set_flood_wait_time(e.value)
         else:
             sended_message.set_message(text=text_message, sending_date=datetime.now(), status=2, channel=ch)
     except BadRequest as e:
         print(str(ch), " SENDING ERROR IS", e.NAME)
-        logging.error(f"{str(ch)}  SENDING ERROR IS {e.NAME}")
+        logging.error(f"{datetime.now()} : {str(ch)}  SENDING ERROR IS {e.NAME}")
         sended_message.set_message(text=text_message, sending_date=datetime.now(), status=2, channel=ch)
     except Forbidden as e:
         print(str(ch), " SENDING ERROR IS", e.NAME)
-        logging.error(f"{str(ch)}  SENDING ERROR IS {e.NAME}")
+        logging.error(f"{datetime.now()} : {str(ch)}  SENDING ERROR IS {e.NAME}")
         sended_message.set_message(text=text_message, sending_date=datetime.now(), status=1, channel=ch)
     except Flood as e:
         print(str(ch), " SENDING ERROR IS", e.NAME)
-        logging.error(f"{str(ch)}  SENDING ERROR IS {e.NAME} WAIT TIME IS {e.value}")
+        logging.error(f"{datetime.now()} : {str(ch)}  SENDING ERROR IS {e.NAME} WAIT TIME IS {e.value}")
         sended_message.set_message(text=text_message, sending_date=datetime.now(), status=3, channel=ch)
         sended_message.set_flood_wait_time(e.value)
     except KeyError as e:
         print(str(ch), " SENDING ERROR IS", str(e))
-        logging.error(f"{str(ch)}  SENDING ERROR IS {str(e)}")
+        logging.error(f"{datetime.now()} : {str(ch)}  SENDING ERROR IS {str(e)}")
         sended_message.set_message(text=text_message, sending_date=datetime.now(), status=5, channel=ch)
     await app.disconnect()
     sended_message.account_name = settings.account
