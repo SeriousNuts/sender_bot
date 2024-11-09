09707705624165ecd954e8f3be52926011117987
Serio <SeriousNuts@yandex.ru>
2024-11-09 17:35:44 +0700

    delete parse_mode

--- run_bot.py
+++ run_bot.py
@@ -4,17 +4,17 @@ import logging
 import sys
 
 from aiogram import Bot
-from aiogram.enums import ParseMode
 
-#from aio_bot.config import TOKEN, TOKEN_TEST
+# from aio_bot.config import TOKEN, TOKEN_TEST
 from aio_bot.handlers import dp
+
 config = configparser.ConfigParser()
 config.read('config.ini')
 TOKEN = config['secrets']['bot_token']
 
 async def main() -> None:
     # Initialize Bot instance with a default parse mode which will be passed to all API calls
-    bot_m = Bot(TOKEN, parse_mode=ParseMode.HTML)
+    bot_m = Bot(TOKEN)
     # And the run events dispatching
     await dp.start_polling(bot_m, skip_updates=False)
 
