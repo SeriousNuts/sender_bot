a7c70fa42716511c39cfabbd6ef8cede557e362c
Serio <SeriousNuts@yandex.ru>
2024-11-28 20:23:03 +0700

    добавление отправки статистики 2.0

--- apscheduller/jobs/sending_job.py
+++ apscheduller/jobs/sending_job.py
@@ -32,9 +32,11 @@ async def get_schedules():
 async def send_messages():
     schedules = await get_schedules()  # получаем список заданий
     tasks_queue = []
+    schedule_uuid_list = []
     for s in schedules:
         accounts = await get_accounts_by_schedule(s)
         schedule_uuid = str(uuid.uuid4())
+        schedule_uuid_list.append(schedule_uuid)
         for acc in accounts:
             app = await get_app_by_session_string(session_string=acc.session_string, app_name=acc.name)
             channels = await get_channels_by_app(app)
@@ -42,19 +44,24 @@ async def send_messages():
                                                                       account_name=acc.name,
                                                                       schedule_owner_id=s.owner_tg_id,
                                                                       schedule_uuid=schedule_uuid)))
-        await send_schedule_stats_to_user(schedule_uuid=schedule_uuid, schedule_owner_id=s.owner_tg_id)
     await asyncio.gather(*tasks_queue)  # ожидаем завершения всех задач на отправку сообщений
-    for s in schedules:
-        s.last_sening = datetime.now()
-        s.next_sending = datetime.now() + timedelta(minutes=s.period)
-        s.status = "work"
-
-    session.commit()
-
+    for index, s in enumerate(schedules):
+        try:
+            s.last_sening = datetime.now()
+            s.next_sending = datetime.now() + timedelta(minutes=s.period)
+            s.status = "work"
+            await send_schedule_stats_to_user(schedule_uuid=schedule_uuid_list[index], schedule_owner_id=s.owner_tg_id)
+        except Exception as e:
+            logging.error(f"Error updating schedule {s.id}: {e}")
+    try:
+        session.commit()
+    except Exception as e:
+        logging.error(f"Error committing session in send message: {e}")
+        session.rollback()
 
 async def send_schedule_stats_to_user(schedule_uuid, schedule_owner_id):
     stats = await get_stats_by_schedule_uuid(schedule_uuid=schedule_uuid)
-    message = (f"Совершена рассылка \n " +
+    message = (f"Совершена рассылка \n" +
                f"Успешно отправлено в {stats.sended_message_count} чатов из {stats.get_all_message_count()} \n" +
                f"В {stats.forbidden_message_count} чатах получен бан \n" +
                f"В {stats.flood_wait_message_count} чатах сообщение не отправлено из-за ограничений флуд фильтра")
