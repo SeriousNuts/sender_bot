806249f3a2e92d003dd738b86d69ecddb0920a74
Serio <SeriousNuts@yandex.ru>
2024-12-03 13:15:46 +0700

    добавил job Отправки отложенных в run_jobs.py

--- run_jobs.py
+++ run_jobs.py
@@ -5,12 +5,14 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from pytz import utc
 
 from apscheduller.jobs.sending_message_job import send_messages
+from apscheduller.jobs.sending_delayd_message_job import send_delayed_messages
 from apscheduller.scheduler_conf import jobstores, executors, job_defaults
 logging.basicConfig(level=logging.INFO, filename="py_log.log", filemode="a",
                     format="%(asctime)s %(levelname)s %(message)s")
 scheduler = AsyncIOScheduler()
 
 scheduler.add_job(send_messages, "interval", seconds=3)
+scheduler.add_job(send_delayed_messages, "interval", seconds=5)
 scheduler.start()
 print("==jobs scheduller started==")
 try:
